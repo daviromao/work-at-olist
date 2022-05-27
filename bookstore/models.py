f53b0bbb8323787e803a2585ecf54ffb876bd1df
daviromao <davi.romao0210@gmail.com>
2022-05-27 19:30:49 -0300

    create book and author model

--- bookstore/models.py
+++ bookstore/models.py
@@ -1,3 +1,26 @@
+from pyexpat import model
+from tabnanny import verbose
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+	name = models.CharField(max_length=255, unique=True)
+
+	class Meta:
+		ordering = ['name']
+
+	def __str__(self):
+		return self.name
+
+
+class Book(models.Model):
+	name = models.CharField(max_length=255)
+	edition = models.PositiveSmallIntegerField(default=1)
+	publication_year = models.PositiveSmallIntegerField()
+	authors = models.ManyToManyField(Author, related_name="books")
+
+	class Meta:
+		ordering = ['-publication_year']
+	
+	def __str__(self):
+		return self.name
