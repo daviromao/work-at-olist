fe8486f7f863861c4cea570f85219113de92b441
daviromao <davi.romao0210@gmail.com>
2022-05-28 18:41:43 -0300

    create cruds and routes to authors and books

--- bookstore/views.py
+++ bookstore/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from bookstore.models import Author, Book
+from bookstore.serializers import AuthorSerializer, BookSerializer
+
+class AuthorViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
