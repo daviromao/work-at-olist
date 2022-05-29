3e60743dac149955b56d4c65e3525307c065ed83
daviromao <davi.romao0210@gmail.com>
2022-05-29 11:09:48 -0300

    fix author name in urls

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -3,5 +3,5 @@ from rest_framework.routers import DefaultRouter
 from bookstore.views import AuthorViewSet, BookViewSet
 
 bookstore_router = DefaultRouter()
-bookstore_router.register('auhtors', AuthorViewSet)
+bookstore_router.register('authors', AuthorViewSet)
 bookstore_router.register('books', BookViewSet)
