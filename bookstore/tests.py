b98066af4df7f2de783fae9b1f953d661049a169
daviromao <davi.romao0210@gmail.com>
2022-05-30 20:33:06 -0300

    create test to author

--- bookstore/tests.py
+++ bookstore/tests.py
@@ -1,3 +1,38 @@
-from django.test import TestCase
+from urllib import response
+from django.urls import reverse
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from bookstore.models import Author, Book
+
+
+class AuthorTests(APITestCase):
+
+    def setUp(self):
+        self.author_one = Author.objects.create(name='Luciano Ramalho')
+        self.author_two = Author.objects.create(name='Robert C. Martin')
+
+    def test_get_authors(self):
+        """
+        Ensure we can read author list
+        """
+
+        url = reverse('author-list')
+
+        response = self.client.get(url, format='json')
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+
+    def test_get_author(self):
+        """
+        Ensure we can read author detail
+        """
+
+        url = reverse('author-detail', args=[1])
+        
+        response = self.client.get(url, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json()['name'], self.author_one.name)
