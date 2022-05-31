16d451c701c5979c16c3a45733dea30208b1535c
daviromao <davi.romao0210@gmail.com>
2022-05-31 09:31:05 -0300

    add optional args --hide to hide output

--- bookstore/management/commands/import_authors.py
+++ bookstore/management/commands/import_authors.py
@@ -8,15 +8,19 @@ class Command(BaseCommand):
 
 	def add_arguments(self, parser):
 		parser.add_argument('csv_path', type=str)
+		parser.add_argument('--hide', action='store_true', help='hide output print')
 
 	def handle(self, *args, **options):
 		csv_path = options['csv_path']
+
+
 
 		with open(csv_path, mode='r', encoding='utf-8') as csv_authors:
 			authors_reader = csv.reader(csv_authors)
 			next(authors_reader, None) #skip header
 
-			self.stdout.write('importing authors from csv file...')
+			if not options['hide']:
+				self.stdout.write('importing authors from csv file...')
 
 			for name in authors_reader:
 				try:
@@ -25,5 +29,6 @@ class Command(BaseCommand):
 				except Exception as e:
 					self.stdout.write(f'author: {name[0][:20]:20} - error: {e}')
 
-			self.stdout.write('\n')
-			self.stdout.write('imported authors!')
+			if not options['hide']:
+				self.stdout.write('\n')
+				self.stdout.write('imported authors!')
