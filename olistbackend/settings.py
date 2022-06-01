583a7a9c591d9a815198ec79f6d3df8237641ba3
daviromao <davi.romao0210@gmail.com>
2022-06-01 17:56:10 -0300

    set debug true and add 127.0.0.1 host

--- olistbackend/settings.py
+++ olistbackend/settings.py
@@ -30,9 +30,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['olistbackend.herokuapp.com']
+ALLOWED_HOSTS = ['olistbackend.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
